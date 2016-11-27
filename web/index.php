2faf313286a0799dcc88a8cca5a1917bad04e4bc
vagrant <vagrant@localhost.localdomain>
2016-11-27 03:41:43 -0500

    Commit.

--- web/index.php
+++ web/index.php
@@ -6,6 +6,9 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+/**
+ * Services
+ */
 $app->register( new Silex\Provider\DoctrineServiceProvider(), array(
 	'db.options'	=> array(
 		'driver'	=> 'pdo_sqlite',
@@ -17,14 +20,46 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 	'twig.path'	=> __DIR__ . '/views',
 ));
 
-//$app['db']->executeUpdate($sql, array('newValue', (int) $id));
+$app->register(new Silex\Provider\AssetServiceProvider(), array(
+		'assets.version' => 'v1',
+		'assets.version_format' => '%s?version=%s',
+		'assets.named_packages' => array(
+			'css' => array('version' => 'css2', 'base_path' => '/whatever-makes-sense'),
+			'images' => array('base_urls' => array('https://img.example.com')),
+		),
+));
 
-$app->get('/', function ( $name ) use ( $app ) {
+/**
+ * Routes
+ */
+$app->get('/', function () use ( $app )
+{
 	$projects	= $app['db']->fetchAll( 'SELECT * FROM projects' );
+	$tools		= $app['db']->fetchAll( 'SELECT * FROM tools' );
 	
 	return $app['twig']->render( 'dashboard/index.twig', array(
-		'projects' => $projects,
+		'projects'	=> $projects,
+		'tools'		=> $tools
 	));
 });
 
+$app->get('/project/install', function ( $id ) use ( $app )
+{
+	$project	= $app['db']->fetchOne( sprintf( 'SELECT * FROM projects WHERE id=%d', $id ) );
+
+	return $app['twig']->render( 'dashboard/index.twig', array(
+			'projects' => $projects,
+	));
+});
+
+$app->get('/tool/install', function ( $id ) use ( $app )
+{
+	$tool	= $app['db']->fetchOne( sprintf( 'SELECT * FROM tools WHERE id=%d', $id ) );
+
+	return $app['twig']->render( 'dashboard/index.twig', array(
+			'projects' => $projects,
+	));
+});
+
+$app['debug'] = true;
 $app->run();
