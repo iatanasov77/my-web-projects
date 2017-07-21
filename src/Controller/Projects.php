fe1b4f892f6811e05cce2aba36b229705440b046
vagrant <vagrant@myprojects.dev>
2017-07-21 05:22:58 -0400

    Create a modal dialog window for the project instalation. Create db columns for  git username and git password.

--- src/Controller/Projects.php
+++ src/Controller/Projects.php
@@ -10,26 +10,26 @@ use VankoSoft\MyProjects\Component\Shell;
 class Projects implements ControllerProviderInterface
 {
 	private $app;
-	
+
 	public function connect( Application $app )
 	{
 		$this->app		= $app;
 		$controllers	= $this->app['controllers_factory'];
-		
+
 		$controllers->get( '/{id}/install', array( $this, 'installAction' ) );
-		
+
 		return $controllers;
 	}
-	
+
 	public function installAction()
 	{
 		$id				= $this->app['request_stack']->getCurrentRequest()->get( 'id', null );
 		$project 		= $this->app ['db']->fetchAssoc( sprintf ( 'SELECT * FROM projects WHERE id=%d', $id ) );
-		
+
 		$projectRoot	= APP_ROOT . '/dir/projects/' . $project['project_root'];
 		mkdir( $projectRoot, 2775, true );
-		
-		if ( $project['git_username'] && $project['git_password'] )
+
+		if ( isset( $project['git_username'] ) && isset( $project['git_password'] ) )
 		{
 			$cmdGitClone	= sprintf(
 				"git clone https://%s:%s@%s %s",
@@ -47,13 +47,13 @@ class Projects implements ControllerProviderInterface
 				$projectRoot
 			);
 		}
-		
+
 		Shell::exec( $cmdGitClone );
-		
+
 		Shell::exec( 'mkvhost' );
-		
+
 		die("EHO");
-		
+
 		return $this->app->redirect( '/' );
 	}
 }
