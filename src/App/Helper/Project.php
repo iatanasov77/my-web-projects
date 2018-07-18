00ae9f3c0a818c174066e145b9bf87f68bbf105a
ivan.atanasov <ivan.atanasov@CPX-JYRS3GF39CP.dir.svc.accenture.com>
2018-07-18 17:44:20 +0300

    Global Project helper functions

---
+++ src/App/Helper/Project.php
@@ -0,0 +1,27 @@
+<?php namespace VankoSoft\MyProjects\Helper;
+
+
+class Project
+{
+
+    public static function exists( $project )
+    {
+        global $app;
+
+        return is_dir( $app ['projects_path'] . $project['project_root'] );
+    }
+
+    public static function installed( $project )
+    {
+        global $app;
+
+        $hosts  = [];
+        if ( file_exists( $app['installed_hosts'] ) )
+        {
+            $json		= file_get_contents( $app['installed_hosts'] );
+            $hosts 		= json_decode( $json, true );
+        }
+
+        return isset( $hosts[$project['id']] );
+    }
+}
