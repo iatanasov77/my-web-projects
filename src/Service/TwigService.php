b3edd5877dfa5f3ec4f554c4bad2be9f03caa963
ghzUser <ghzUser@DESKTOP-JGJNF9D>
2019-09-03 00:15:34 +0300

    #12 Create symfony cli command to create a virtual host

---
+++ src/Service/TwigService.php
@@ -0,0 +1,13 @@
+<?php namespace App\Service;
+
+use Symfony\Component\HttpKernel\KernelInterface;
+
+class TwigService extends \Twig_Environment {
+
+    public function __construct( KernelInterface $kernel )
+    {
+        $loader = new \Twig_Loader_Filesystem( $kernel->getProjectDir() );
+        
+        parent::__construct( $loader );
+    }
+}
