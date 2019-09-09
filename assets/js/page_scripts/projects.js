99bdef08c8e8dc8165f43e36107764af5a401bcc
ghzUser <ghzUser@DESKTOP-JGJNF9D>
2019-09-10 00:22:40 +0300

    Commit!

--- assets/js/page_scripts/projects.js
+++ assets/js/page_scripts/projects.js
@@ -1,10 +1,1 @@
-$( '#manual-ajax' ).click( function( event )
-{
-	this.blur(); // Manually remove focus from clicked button.
-	var href	= this.attr( 'data-url' );
-	
-	$.get( href, function( html )
-	{
-		$( html ).appendTo( 'body' ).modal();
-	});
-});
+
