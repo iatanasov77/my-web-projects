cdc9dcddc3cd078851a84e15036cf109fb144539
ghzUser <ghzUser@DESKTOP-JGJNF9D>
2019-09-19 08:27:26 +0300

    #19 HUI

--- vagrant.d/provision/install_puppet.sh
+++ vagrant.d/provision/install_puppet.sh
@@ -1,17 +1,48 @@
 #!/bin/bash
 
 echo "installing Puppet"
-wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
-sudo dpkg -i puppetlabs-release-trusty.deb
-sudo apt-get update
-sudo apt-get -y install puppet
+source /vagrant/vagrant.d/provision/detect_linux.sh
 
-echo "ensure puppet service is running"
-sudo puppet resource service puppet ensure=running enable=true
+if [ "$OS" == "Ubuntu" ]; then
+	wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
+	sudo dpkg -i puppetlabs-release-trusty.deb
+	sudo apt-get update
+	sudo apt-get -y install puppet
+	
+	echo "ensure puppet service is running"
+	sudo puppet resource service puppet ensure=running enable=true
+	
+	echo "ensure puppet service is running for standalone install"
+	sudo puppet resource cron puppet-apply ensure=present user=root minute=30 command='/usr/bin/puppet apply $(puppet apply --configprint manifest)'
+	
+	# Workaround: Puppet cannot find installed modules
+	echo "Remove /usr/share/puppet/modules and link that dir to current installed modules ./modules"
+	sudo rm -rf /usr/share/puppet/modules
+	sudo ln -sf /vagrant/vagrant.d/puppet/modules /usr/share/puppet/modules
+	
+	#sudo apt-get -y install ruby-dev
+	sudo apt-get -y install librarian-puppet
+	#sudo apt-get -y install curl
+else
+	sudo rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
+	sudo yum -y install puppet
+	
+	sudo ln -s /opt/puppetlabs/bin/puppet /usr/local/bin/puppet
+	
+	echo "ensure puppet service is running"
+	sudo /opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true
+	
+	echo "ensure puppet service is running for standalone install"
+	sudo /opt/puppetlabs/bin/puppet resource cron puppet-apply ensure=present user=root minute=30 command='/usr/bin/puppet apply $(puppet apply --configprint manifest)'
 
-echo "ensure puppet service is running for standalone install"
-sudo puppet resource cron puppet-apply ensure=present user=root minute=30 command='/usr/bin/puppet apply $(puppet apply --configprint manifest)'
+	# Workaround: Puppet cannot find installed modules
+	echo "Remove /usr/share/puppet/modules and link that dir to current installed modules ./modules"
+	sudo rm -rf /opt/puppetlabs/puppet/modules
+	sudo ln -sf /vagrant/vagrant.d/puppet/modules /opt/puppetlabs/puppet/modules
 
-#sudo apt-get -y install ruby-dev
-sudo apt-get -y install librarian-puppet
-#sudo apt-get -y install curl
+	########################################################################
+	# NOT USE LIBRARIAN, AT NOW USED GIT SUBMODULES TO ADD PUPPET MODULES
+	########################################################################
+	sudo yum install gem -y
+	sudo gem install librarian-puppet
+fi
