a70ab4ec7ccdb512178b36e669f1de1f892676b1
Ivan Atanasov (Accenture) <ivan.atanasov@accenture.com>
2019-10-23 17:51:46 +0300

    [Vagrant Machine] YAML config for apache and php modules MySql-2.6.x But Apache cannot start

--- vagrant.d/provision/init.sh
+++ vagrant.d/provision/init.sh
@@ -6,7 +6,9 @@ if [ $ID == "centos" ]; then
     rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY*
     yum -y install epel-release
     
+    rpm -Uvh http://repo.mysql.com/mysql-community-release-el6-5.noarch.rpm
     rpm -ivh http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+    
     yum -y install yum-utils
     
     #########################################################################
@@ -16,10 +18,12 @@ if [ $ID == "centos" ]; then
     # eval $ENABLE_REPO
     yum-config-manager --enable remi-php72
     
+    yum install -y mysql mysql-server
     yum install -y php mod_php php-common php-opcache php-mcrypt php-cli php-gd php-curl php-mysql
     
     if [ ! -f /etc/httpd/modules/libphp7.2.so ]; then
-        ln -s /usr/lib64/httpd/modules/libphp7.so /etc/httpd/modules/libphp7.2.so
+        ln -s /usr/lib64/httpd/modules/libphp7.so /usr/lib64/httpd/modules/libphp7.2.so
+        ln -s /usr/lib64/httpd/modules/libphp7.so /usr/lib64/httpd/modules/libphp.so
     fi
 else
 	apt-get update -y
