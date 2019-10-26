711262de9f275e31bb3224f8b28fa09dbe444432
Ivan Atanasov <i.atanasov77@gmail.com>
2019-10-26 15:59:11 +0300

    Commit

--- vagrant.d/provision/init.sh
+++ vagrant.d/provision/init.sh
@@ -6,19 +6,18 @@ if [ $ID == "centos" ]; then
     rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY*
     yum -y install epel-release
     
-    rpm -Uvh http://repo.mysql.com/mysql-community-release-el6-5.noarch.rpm
+    # MySql 5.6
+    #rpm -Uvh http://repo.mysql.com/mysql-community-release-el6-5.noarch.rpm
+    #yum install -y mysql mysql-server
+    
+    # MySql 5.7
+    rpm -Uvh https://repo.mysql.com/mysql80-community-release-el7-3.noarch.rpm
+    sed -i 's/enabled=1/enabled=0/' /etc/yum.repos.d/mysql-community.repo
+    yum --enablerepo=mysql57-community install -y mysql-community-server
+    
+    # PHP7
     rpm -ivh http://rpms.remirepo.net/enterprise/remi-release-7.rpm
-    
-    yum -y install yum-utils
-    
-    #########################################################################
-    # USE PHP VERSION FROM .env
-    #########################################################################
-    # ENABLE_REPO = "yum-config-manager --enable remi-php${PHP_VERSION//.}"
-    # eval $ENABLE_REPO
     yum-config-manager --enable remi-php72
-    
-    yum install -y mysql mysql-server
     yum install -y php mod_php php-common php-opcache php-mcrypt php-cli php-gd php-curl php-mysql
     
     if [ ! -f /etc/httpd/modules/libphp7.2.so ]; then
