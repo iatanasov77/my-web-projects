4bde2379560275758569946fc4c0e69b94d37db5
GhzUser <GhzUser@GhzUser-PC>
2019-11-01 19:21:18 +0200

    Commmit

--- config/bundles.php
+++ config/bundles.php
@@ -4,7 +4,6 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
