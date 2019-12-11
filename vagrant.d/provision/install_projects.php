f82a50844949be06cf434129fb20c9dcb28ff69f
Ivan Atanasov (Accenture) <ivan.atanasov@accenture.com>
2019-12-11 14:21:19 +0200

    Commit

--- vagrant.d/provision/install_projects.php
+++ vagrant.d/provision/install_projects.php
@@ -2,14 +2,22 @@
 <?php
 chdir( '/vagrant' );
 
-$installedHosts = json_decode( file_get_contents( 'installed_hosts.json' ) );
+exec( "mysqladmin -uroot -pvagrant create VsMyProjects" );
+exec( "mysql -uroot -pvagrant VsMyProjects < /vagrant/doc/sql/VsMyProjects-20191206.sql" );
+
+$installedHosts = json_decode( file_get_contents( 'installed_hosts.json' ), true );
 foreach( $installedHosts as $host ) {
     //var_dump( $host ); die;
-    exec( sprintf( "bin/console vs:mkvhost -s%s -d%s", $host->hostName, $host->documentRoot ) );
+    exec( sprintf( "bin/console vs:mkvhost -s%s -d%s", $host['hostName'], $host['documentRoot'] ) );
     
     // Create storage dir out of shared folder
-    if ( $host->createStorageDir ) {
-        exec( sprintf( 'mkdir /var/www/%s', $host->hostName ) );
-        exec( sprintf( 'chmod -R 0777 /var/www/%s', $host->hostName ) );
+    if ( isset( $host['storageDir'] ) ) {
+        exec( sprintf( "mkdir %s", $host['storageDir'] ) );
+        exec( sprintf( "chmod -R 0777 %s", $host['storageDir'] ) );
+    }
+    
+    // Create database
+    if ( isset( $host['database'] ) ) {
+        exec( sprintf( "mysqladmin -uroot -pvagrant create %s", $host['database'] ) );
     }
 }
