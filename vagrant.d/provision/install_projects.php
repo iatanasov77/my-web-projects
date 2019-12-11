035ea1459e659550984ad4721358f4b4287ca6b0
Ivan Atanasov (Accenture) <ivan.atanasov@accenture.com>
2019-12-11 11:48:14 +0200

    Commit

--- vagrant.d/provision/install_projects.php
+++ vagrant.d/provision/install_projects.php
@@ -1,10 +1,15 @@
 #!/usr/bin/php
 <?php
-
 chdir( '/vagrant' );
 
 $installedHosts = json_decode( file_get_contents( 'installed_hosts.json' ) );
 foreach( $installedHosts as $host ) {
     //var_dump( $host ); die;
-    exec( sprintf( "bin/console vs:mkvhost -s%s -d%s", $host->hostName, $host->documentRoot ) );    
+    exec( sprintf( "bin/console vs:mkvhost -s%s -d%s", $host->hostName, $host->documentRoot ) );
+    
+    // Create storage dir out of shared folder
+    if ( $host->createStorageDir ) {
+        exec( sprintf( 'mkdir /var/www/%s', $host->hostName ) );
+        exec( sprintf( 'chmod -R 0777 /var/www/%s', $host->hostName ) );
+    }
 }
