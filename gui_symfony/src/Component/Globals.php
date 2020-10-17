de7c8b98fdd376c73d659d76596f8628825e50f2
Ivan I. Atanasov <i.atanasov77@gmail.com>
2020-10-17 06:45:49 +0000

    Start functionality of Installation of the Predefined project types.

--- gui_symfony/src/Component/Globals.php
+++ gui_symfony/src/Component/Globals.php
@@ -4,6 +4,8 @@ namespace App\Component;
 class Globals
 {
     // Statuses
-    const STATUS_OK     = 'ok';
-    const STATUS_ERROR  = 'error';
+    const STATUS_OK                 = 'ok';
+    const STATUS_ERROR              = 'error';
+    
+    const STATUS_ERROR_TYPE_ALERT   = 'alert';
 }
