bfb6495b9aa41532d2c0c740217134814f9fae5f
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-01-12 19:27:00 +0000

    [VAGRANT] Enable PowerTools Repo for CentOs 8.

--- vagrant.d/puppet/manifests/default.pp
+++ vagrant.d/puppet/manifests/default.pp
@@ -14,6 +14,19 @@ Exec {
 node default
 {
 	include stdlib
+	if $::operatingsystem == 'centos' and $::operatingsystemmajrelease == '8' {
+	   	package { 'dnf-plugins-core':
+	        ensure => present,
+	    }
+
+		yumrepo { 'PowerTools':
+			ensure      => 'present',
+			mirrorlist 	=> 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=PowerTools&infra=$infra',
+			enabled     => 1,
+			gpgcheck 	=> 0,
+			require		=> Package['dnf-plugins-core'],
+		}
+	}
 	
 	# In Puppetfile:
     # mod 'ghoneycutt-ssh', '3.57.0' 
